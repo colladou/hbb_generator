12aa388e483c7859d334957fc9215a5eb3ca0e78
Julian Collado <colladou@uci.edu>
2017-10-05 13:14:53 -0700

    changed name of data file

--- generator.py
+++ generator.py
@@ -144,6 +144,6 @@ def my_generator(file_name, set_name, batch_size=1):
             yield data_batch
 
 if __name__ == "__main__":
-    gen_1 = my_generator('small_test_data_signal.h5', 'hl_tracks')
+    gen_1 = my_generator('small_test_raw_data_signal.h5', 'hl_tracks')
     print(gen_1.next())
      
