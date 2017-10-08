286135634e3d5553ff960d4280131fa670e08418
Julian Collado <colladou@uci.edu>
2017-10-07 20:35:12 -0700

    included file list for signal and background as well as a function to do the predictions

--- calculate_auc_numpy.py
+++ calculate_auc_numpy.py
@@ -46,18 +46,43 @@ def get_predictions_from_file_list(model, file_names, feature, load_path='./'):
         assert predictions.shape[0] == weights.shape[0], weights.shape[0]
     return [predictions, weights]
 
+
 feature = 'hl_tracks'
 
 model_name = get_model_name(feature)
 model = keras.models.load_model("./models/" + model_name)
 
 batch_size = 100
+
+bg_file_names = ['d301488_j1.h5', 'd301489_j2.h5', 'd301490_j3.h5', 'd301491_j4.h5',
+               'd301492_j5.h5', 'd301493_j6.h5', 'd301494_j7.h5', 'd301495_j8.h5',
+               'd301496_j9.h5', 'd301497_j10.h5', 'd301498_j11.h5', 'd301499_j12.h5',
+               'd301500_j13.h5', 'd301501_j14.h5', 'd301502_j15.h5', 'd301503_j16.h5',
+               'd301504_j17.h5', 'd301505_j18.h5', 'd301506_j19.h5', 'd301507_j20.h5']
+s_file_names = [#'d361021_j27.h5', 
+                'd361022_j28.h5', 'd361023_j29.h5','d361024_j30.h5', 
+                'd361025_j31.h5', 'd361026_j32.h5', 'd361027_j33.h5', 'd361028_j34.h5',
+                'd361029_j35.h5', 'd361030_j36.h5', 'd361031_j37.h5', 'd361032_j38.h5']
+
 file_names = ['d301488_j1.h5', 'd301489_j2.h5']
 load_path = '/baldig/physicsprojects/atlas/hbb/raw_data/v_3/'
 
-predictions, weights = get_predictions_from_file_list(model, file_names, feature, load_path)
-label = 1
-test_y = np.ones_like(predictions) * label
+s_predictions, s_weights = get_predictions_from_file_list(model, s_file_names, feature, load_path)
+s_test_y = np.ones_like(predictions)
+
+bg_predictions, bg_weights = get_predictions_from_file_list(model, bg_file_names, feature, load_path)
+bg_test_y = np.ones_like(predictions) * 0
+
+print(s_predictions.shape, s_test_y.shape, s_weights.shape)
+print(bg_predictions.shape, bg_test_y.shape, bg_weights.shape)
+
+predictions = np.hstack((s_predictions, bg_predictions))
+weights = np.hstack((s_weights, bg_weights))
+test_y = np.hstack((s_test_y, bg_test_y))
+
+assert predictions.shape[0] == s_predictions.shape[0] + bg_predictions.shape[0]
+assert weights.shape[0] == s_weights.shape[0] + bg_weights.shape[0]
+assert test_y.shape[0] == s_test_y.shape[0] + bg_test_y.shape[0]
 
 predictions = predictions.reshape(predictions.shape[0],)
 weights = weights.reshape(weights.shape[0],)
@@ -71,8 +96,6 @@ np.save("./auc/%s_test_%s_predictions.npy" % (feature, model_name), predictions)
 print("saved params")
 
 print(test_y.shape, predictions.shape)
-
-assert 1==0, "finished"
 
 print('Calculating unweighted AUC')
 print(metrics.roc_auc_score(test_y, predictions))
