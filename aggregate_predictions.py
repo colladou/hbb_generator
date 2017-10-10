4264eafe465e09b72384c275a2ff2adf5d29d98a
Dan Guest <dguest@cern.ch>
2017-10-10 22:42:46 +0200

    Split into a predict and aggregate stage
    Running prediction is very slow, so this commit splits it into two
    steps: one which runs over each file and calculates predictions for
    each of them, and another which aggregates the predictions and
    calculates a ROC curve.

---
+++ aggregate_predictions.py
@@ -0,0 +1,67 @@
+from file_names import s_file_names
+from file_names import bg_file_names
+import h5py
+from os.path import join, isfile, isdir
+import numpy as np
+from sklearn import metrics
+
+def get_predictions_and_weights(name_list):
+    pred_list = []
+    weights_list = []
+    for fname in name_list:
+        fpath = join('outputs', fname)
+        if not isfile(fpath):
+            print('{} not found, skipping...'.format(fname))
+            continue
+        with h5py.File(fpath, 'r') as h5file:
+            pred_list.append(np.asarray(h5file['predictions']))
+            weights_list.append(np.asarray(h5file['weights']))
+    return np.hstack(pred_list), np.hstack(weights_list)
+
+s_predictions, s_weights = get_predictions_and_weights(s_file_names)
+bg_predictions, bg_weights = get_predictions_and_weights(bg_file_names)
+
+s_test_y = np.ones_like(s_predictions) * 1
+bg_test_y = np.ones_like(bg_predictions) * 0
+
+print(s_predictions.shape, s_test_y.shape, s_weights.shape)
+print(bg_predictions.shape, bg_test_y.shape, bg_weights.shape)
+
+predictions = np.concatenate((s_predictions, bg_predictions))
+weights = np.concatenate((s_weights, bg_weights))
+test_y = np.concatenate((s_test_y, bg_test_y))
+
+assert predictions.shape[0] == s_predictions.shape[0] + bg_predictions.shape[0]
+assert weights.shape[0] == s_weights.shape[0] + bg_weights.shape[0]
+assert test_y.shape[0] == s_test_y.shape[0] + bg_test_y.shape[0]
+
+predictions = predictions.reshape(predictions.shape[0],)
+weights = weights.reshape(weights.shape[0],)
+
+print(predictions.shape, test_y.shape, weights.shape)
+
+assert predictions.shape[0] == test_y.shape[0], predictions.shape[0]
+
+auc_dir = 'auc'
+feature = 'hl_tracks'
+if not isdir(auc_dir):
+    os.mkdir(auc_dir)
+np.save(join(auc_dir, "%s_test_y.npy" % feature), test_y)
+np.save(join(auc_dir,"%s_test_predictions.npy" % feature), predictions)
+print("saved params")
+
+print(test_y.shape, predictions.shape)
+
+print('Calculating unweighted AUC')
+print(metrics.roc_auc_score(test_y, predictions))
+
+print('Calculating weighted AUC')
+w_auc = metrics.roc_auc_score(test_y, predictions, sample_weight=weights)
+print(w_auc)
+np.savetxt('./auc/auc_%s.csv' % feature, [w_auc,], delimiter=',')
+
+fpr, tpr, _ = metrics.roc_curve(test_y, predictions, sample_weight=weights)
+
+np.save('./auc/tpr_%s.npy' % (feature), tpr)
+np.save('./auc/fpr_%s.npy' % (feature), fpr)
+
--- calculate_auc_numpy.py
+++ calculate_auc_numpy.py
@@ -11,15 +11,17 @@ from keras.models import model_from_json
 from keras.models import Sequential
 from keras.optimizers import SGD
 from keras.layers.core import Activation, Dense, Dropout
-from sklearn import metrics
 import sys
 import numpy as np
 import h5py
 import math
 from generator import my_generator
 from generator import get_num_samples, get_weights
-from os.path import isdir, join
 import argparse
+from os.path import join, isdir, basename
+
+# the file lists are in a common file now
+from file_names import s_file_names, bg_file_names
 
 # be able to predict only on smaller number of samples of the file
 
@@ -32,28 +34,39 @@ def get_args():
 def get_model_name(feature):
     return '%s_model.h5' % feature
 
-def get_predictions_from_file_list(model, file_names, feature, load_path='./', sub_sample=100):
+def get_predictions_from_file_list(model, file_names, feature, load_path='./', sub_sample=100, out_file_path='outputs'):
+    if not isdir(out_file_path):
+        os.mkdir(out_file_path)
     predictions = None
     weights = None
     for file_name in file_names:
         print("Making predictions for: ", file_name)
-        file_name = load_path + file_name
+        file_name = join(load_path,file_name)
         with h5py.File(file_name, 'r') as open_file:
             num_samples = int(get_num_samples(open_file) * (sub_sample/100.0))
             steps = math.ceil(num_samples/(batch_size*1.0))
             file_weights = get_weights(open_file)
             file_weights = file_weights[0:steps*batch_size]
-        gen = my_generator(file_name, feature, batch_size)
-        print("running generator in {} steps".format(steps))
-        file_predictions = model.predict_generator(gen, steps)
-        if predictions is None:
-            predictions = file_predictions
-            weights = file_weights
-        else:
-            predictions = np.vstack((predictions, file_predictions))
-            weights = np.hstack((weights, file_weights))
-        assert predictions.shape[0] == weights.shape[0], [predictions.shape[0], weights.shape[0]]
-    return [predictions, weights]
+
+        out_path = join(out_file_path, basename(file_name))
+        with h5py.File(out_path, 'w') as out_file:
+            predictions = out_file.create_dataset(
+                'predictions', (0,), maxshape=(None,),
+                chunks=(batch_size,), dtype=float)
+            weights = out_file.create_dataset(
+                'weights', (0,), maxshape=(None,),
+                chunks=(batch_size,), dtype=float)
+            offset = 0
+            for batch in my_generator(file_name, feature, batch_size):
+                new_offset = offset + batch.shape[0]
+                # build a slice object
+                batch_slice = slice(offset, new_offset)
+                predictions.resize(new_offset, 0)
+                batch_prediction = model.predict(batch)
+                predictions[batch_slice] = batch_prediction[0]
+                weights.resize(new_offset, 0)
+                weights[batch_slice] = file_weights[batch_slice]
+                offset = new_offset
 
 args = get_args()
 
@@ -64,20 +77,11 @@ model = keras.models.load_model("./models/" + model_name)
 
 batch_size = 1000
 
-s_file_names = ['d301488_j1.h5', 'd301489_j2.h5', 'd301490_j3.h5', 'd301491_j4.h5',
-                 'd301492_j5.h5', 'd301493_j6.h5', 'd301494_j7.h5', 'd301495_j8.h5',
-                 'd301496_j9.h5', 'd301497_j10.h5', 'd301498_j11.h5', 'd301499_j12.h5',
-                 'd301500_j13.h5', 'd301501_j14.h5', 'd301502_j15.h5', 'd301503_j16.h5',
-                 'd301504_j17.h5', 'd301505_j18.h5', 'd301506_j19.h5', 'd301507_j20.h5']
-bg_file_names = [#'d361021_j27.h5',
-                'd361022_j28.h5', 'd361023_j29.h5','d361024_j30.h5',
-                'd361025_j31.h5', 'd361026_j32.h5', 'd361027_j33.h5', 'd361028_j34.h5',
-                'd361029_j35.h5', 'd361030_j36.h5', 'd361031_j37.h5', 'd361032_j38.h5']
 
 if args.mode == 'local':
     load_path = 'data/'
-    s_file_names = ['d301488_j1.h5']
-    bg_file_names = ['d361022_j28.h5']
+    s_file_names = s_file_names[0:1]
+    bg_file_names = bg_file_names[0:1]
 
 elif args.mode == 'julian':
     load_path = '/baldig/physicsprojects/atlas/hbb/raw_data/v_3/'
@@ -85,49 +89,6 @@ elif args.mode == 'julian':
 elif args.mode == 'dan':
     load_path = '/home/dguest/bookmarks/hbb/hbb/v3/data/'
 
-s_predictions, s_weights = get_predictions_from_file_list(model, s_file_names, feature, load_path)
-s_test_y = np.ones_like(s_predictions) * 0
-
-bg_predictions, bg_weights = get_predictions_from_file_list(model, bg_file_names, feature, load_path, sub_sample=10)
-bg_test_y = np.ones_like(bg_predictions) * 1
-
-print(s_predictions.shape, s_test_y.shape, s_weights.shape)
-print(bg_predictions.shape, bg_test_y.shape, bg_weights.shape)
-
-predictions = np.vstack((s_predictions, bg_predictions))
-weights = np.hstack((s_weights, bg_weights))
-test_y = np.vstack((s_test_y, bg_test_y))
-
-assert predictions.shape[0] == s_predictions.shape[0] + bg_predictions.shape[0]
-assert weights.shape[0] == s_weights.shape[0] + bg_weights.shape[0]
-assert test_y.shape[0] == s_test_y.shape[0] + bg_test_y.shape[0]
-
-predictions = predictions.reshape(predictions.shape[0],)
-weights = weights.reshape(weights.shape[0],)
-
-print(predictions.shape, test_y.shape, weights.shape)
-
-assert predictions.shape[0] == test_y.shape[0], predictions.shape[0]
-
-auc_dir = 'auc'
-if not isdir(auc_dir):
-    os.mkdir(auc_dir)
-np.save(join(auc_dir, "%s_test_%s_y.npy" % (feature, model_name)), test_y)
-np.save(join(auc_dir,"%s_test_%s_predictions.npy" % (feature, model_name)), predictions)
-print("saved params")
-
-print(test_y.shape, predictions.shape)
-
-print('Calculating unweighted AUC')
-print(metrics.roc_auc_score(test_y, predictions))
-
-print('Calculating weighted AUC')
-w_auc = metrics.roc_auc_score(test_y, predictions, sample_weight=weights)
-print(w_auc)
-np.savetxt('./auc/auc_%s.csv' % feature, [w_auc,], delimiter=',')
-
-fpr, tpr, _ = metrics.roc_curve(test_y, predictions, sample_weight=weights)
-
-np.save('./auc/tpr_%s.npy' % (feature), tpr)
-np.save('./auc/fpr_%s.npy' % (feature), fpr)
+get_predictions_from_file_list(model, s_file_names, feature, load_path)
+get_predictions_from_file_list(model, bg_file_names, feature, load_path, sub_sample=10)
 
