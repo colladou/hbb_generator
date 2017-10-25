c0c9c4cdc8266bb4760bc16ede6d9dfab4865189
Dan Guest <dguest@cern.ch>
2017-10-25 10:49:19 +0200

    get ROC curve plotting working from the histograms

--- draw_histograms.py
+++ draw_histograms.py
@@ -2,9 +2,12 @@
 
 import h5py
 import argparse
+from os.path import join
+from os import mkdir
 import numpy as np
 from itertools import product
 from argparse import ArgumentParser
+from mpl import Canvas
 
 def get_args():
     parser = ArgumentParser()
@@ -16,14 +19,21 @@ def compute_roc(h5file, discrim, variable, window):
     signal = h5file['signal']['{}_vs_{}'.format(variable, discrim)]
     bg = h5file['background']['{}_vs_{}'.format(variable, discrim)]
     n_bins = signal.shape[0] - 2
-    axis_range = np.linspace(*bg.attrs['axis_range'][0], n_bins)
+    axis_range = np.linspace(*bg.attrs['axis_range'][0], n_bins+1)
+    print(axis_range.shape)
     low, high = window
-    valid = (axis_range < high) & (axis_range > low)
-    sig_squash = signal[valid, :].sum(axis=1)
-    bg_squash = bg[valid, :].sum(axis=1)
-    tpr = sig_squash / sig_squash.sum()
-    fpr = bg_squash / bg_squash.sum()
+    valid = (axis_range[1:] < high) & (axis_range[:-1] > low)
+    print(valid)
+    sig_squash = signal[valid, :].sum(axis=0)
+    bg_squash = bg[valid, :].sum(axis=0)
+    tpr = sig_squash[::-1].cumsum() / sig_squash.sum()
+    fpr = bg_squash[::-1].cumsum() / bg_squash.sum()
     return tpr, fpr
+
+def draw_roc(can, tpr, fpr, title):
+    valid = tpr > 0.2
+    rej = 1/fpr[valid]
+    can.ax.plot(tpr[valid], rej, label=title)
 
 WINDOWS = [
     ('jets_pt', 250, 400),
@@ -38,11 +48,16 @@ def run():
     args = get_args()
     rates = {}
     with h5py.File(args.input_file, 'r') as h5_file:
-        for discrim, window in product(DISCRIM, WINDOWS):
-            var, *bounds = window
-            tpr, fpr = compute_roc(h5_file, discrim, var, bounds)
-            title = r'{}: {} -- {}'.format(*window)
-
+        for window in WINDOWS:
+            path = join(args.output_dir, '{}-{}-{}.pdf'.format(*window))
+            with Canvas(path) as can:
+                for discrim in DISCRIM:
+                    var, *bounds = window
+                    tpr, fpr = compute_roc(h5_file, discrim, var, bounds)
+                    title = r'{}: {} -- {}'.format(discrim,*window[1:])
+                    draw_roc(can, tpr, fpr, title)
+                can.ax.legend()
+                can.ax.set_yscale('log')
 
 if __name__ == '__main__':
     run()
