015bd56542b93a6fe52bcf5b56b8208d454aa2ab
Dan Guest <dguest@cern.ch>
2017-11-20 20:17:09 +0100

    Add name convention converter (-> ATLAS EDM)

--- build_varspec_file.py
+++ build_varspec_file.py
@@ -20,6 +20,57 @@ def get_args():
     parser.add_argument('set_name', default='hl_tracks', **d)
     return parser.parse_args()
 
+ntup_to_edm_taggers = {
+    'ip2d': 'IP2D',
+    'ip3d': 'IP3D',
+    '_jf': '_JetFitter',
+    '_sv1': '_SV1',
+}
+jf_ntup_to_edm_vars = {
+    '_dr': '_dRFlightDir',
+    '_efc': '_energyFraction',
+    '_m': '_mass',
+    '_n2t': '_N2Tpair',
+    '_ntrkAtVx': '_nTracksAtVtx',
+    '_nvtx': '_nVTX',
+    '_nvtx1t': '_nSingleTracks',
+    '_sig3d': '_significance3d',
+    '_deta': '_deltaeta',
+    '_dphi': '_deltaphi',
+}
+sv_ntup_to_edm_vars = {
+    '_dR': '_deltaR',
+    '_efc': '_efracsvx',
+    '_Lxyz': '_L3d',
+    '_Lxy': '_Lxy',
+    '_m': '_massvx',
+    '_n2t': '_N2Tpair',
+    '_ntrkv': '_NGTinSvx',
+    '_normdist': '_normdist',
+}
+
+energy_corrections = {
+    'mass': 1e3,
+    'massvx': 1e3,
+    'pt': 1e3,
+}
+
+def correct_entry(entry):
+    for old, new in ntup_to_edm_taggers.items():
+        entry['name'] = entry['name'].replace(old, new)
+    if 'IP2D' in entry['name'] or 'IP3D' in entry['name']:
+        for old, new in sv_ntup_to_edm_vars.items():
+            if entry['name'].endswith(old):
+                entry['name'] = entry['name'].replace(old,new)
+    if 'JetFitter' in entry['name']:
+        for old, new in jf_ntup_to_edm_vars.items():
+            if entry['name'].endswith(old):
+                entry['name'] = entry['name'].replace(old,new)
+    for key in energy_corrections:
+        if entry['name'].endswith(key):
+            entry['offset'] *= energy_corrections[key]
+            entry['scale'] *= energy_corrections[key]
+
 def run():
     args = get_args()
     offset_and_scale = zip(-np.load(args.mean_vector),
@@ -36,12 +87,13 @@ def run():
             offset, scale = next(norm_iterator)
             if isinf(scale):
                 assert offset == 0.0
-                scale = 1.0
+                scale = 0.0
             entry = {
                 'name': compound_name,
                 'offset': float(offset),
                 'scale': float(scale)
             }
+            correct_entry(entry)
             out_dict['inputs'].append(entry)
     json.dump(out_dict, sys.stdout, indent=2)
 
